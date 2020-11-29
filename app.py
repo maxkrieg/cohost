3e92ca15339c71a1066819ab4d0c5faada4a875b
Max Krieg <maxwell.krieg@gmail.com>
2020-11-29 13:17:45 -0700

    add url prefix, add cors

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from flask_restful import Api
 from resources.errors import errors
 from resources.users import UsersApi, UserApi
@@ -16,12 +17,13 @@ def create_app():
     app.config.from_envvar("ENV_FILE_LOCATION")
     Bcrypt(app)
     JWTManager(app)
+    CORS(app)
     api = Api(app, errors=errors)
-    api.add_resource(UsersApi, "/admin/users")
-    api.add_resource(UserApi, "/admin/users/<string:user_handle>")
-    api.add_resource(EventsApi, "/events")
-    api.add_resource(SignupApi, "/auth/signup")
-    api.add_resource(LoginApi, "/auth/login")
+    api.add_resource(UsersApi, "/api/admin/users")
+    api.add_resource(UserApi, "/api/admin/users/<string:user_handle>")
+    api.add_resource(EventsApi, "/api/events")
+    api.add_resource(SignupApi, "/api/signup")
+    api.add_resource(LoginApi, "/api/login")
     db.init_app(app)
 
     return app
